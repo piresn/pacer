b0f440a402bc89ae480c51d88204b530c045f549
Nuno Pires <mail@nunopires.net>
2022-09-10 21:51:20 +0200

    add pace class

--- helpers.py
+++ helpers.py
@@ -1,12 +1,38 @@
 def kmh_to_seckm(x):
      return round(3600/x)
 
-def seckm_to_string(x):
-    min = f'{x//60}'
-    sec = f'{x%60}'.zfill(2)
-    return f'{min}:{sec}'
+def seckm_to_string(x, decimals):
+    min = f'{int(x//60)}'
+    sec = x%60
+
+    if decimals:
+        sec_align = f'{int(sec)}'.zfill(2) + '.' + f'{round(10*(sec-int(sec)))}'
+    else:
+        sec_align = f'{round(sec)}'.zfill(2)
+    return f'{min}:{sec_align}'
 
 def kmh_to_pace(x):
     x = kmh_to_seckm(x)
     x = seckm_to_string(x)
     return x
+
+class Pace:
+    """
+    Creates paces, store as seconds per km and print as min:sec per km.
+    """
+    def __init__(self):
+        self.pace=None
+
+    def pace_from_kmh(self, kmh):
+        self.pace=kmh_to_seckm(kmh)
+
+    def pace_from_distance(self, meters, seconds, hours=0, minutes=0):
+        time = seconds+60*minutes+3600*hours
+        km = meters/1000
+        self.pace = time/km
+
+    def print(self):
+        return seckm_to_string(self.pace, decimals=False)
+
+
+
