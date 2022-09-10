462f257c086369aca875a8f46fd9efbf6887b2c6
Nuno Pires <mail@nunopires.net>
2022-09-10 15:28:10 +0200

    add user percentage

--- app.py
+++ app.py
@@ -8,16 +8,22 @@ st.title('Pace predictor')
 
 records = pd.read_csv('records.csv')
 
-records = records.melt(id_vars='Distance', var_name='Gender', value_name='Speed')
+user_percentage = st.number_input('Set a speed percentage:',
+    min_value=1.0, max_value=110.0, value=68.0, step=0.1)
+
+records['User'] = records['Men'] * user_percentage/100
+
+records = records.melt(id_vars='Distance', var_name='Group', value_name='Speed')
 
 records['Pace'] = records['Speed'].apply(kmh_to_pace)
-
 
 a = alt.Chart(records).mark_circle().encode(
     x=alt.Y('Distance', scale=alt.Scale(type="log")),
     y=alt.Y('Speed', scale=alt.Scale(type="log")),
-    color='Gender',
+    color='Group',
     tooltip=['Distance', 'Speed', 'Pace']
 )
 
 st.write(a)
+
+st.write(records[records.Group == 'User'][['Distance', 'Pace']])
