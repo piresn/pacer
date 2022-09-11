471893aa42b5912233f79392a621a1524802c9b6
Nuno Pires <mail@nunopires.net>
2022-09-11 14:14:25 +0200

    add Pace class

--- app.py
+++ app.py
@@ -8,14 +8,35 @@ st.title('Pace predictor')
 
 records = pd.read_csv('records.csv')
 
+p=Pace()
+p.pace_from_distance(meters=5000, minutes=18, seconds=31)
+p.print(decimals=3)
+####################################################
+
+user_distance = st.selectbox('distance',
+['100m', '200m', '400m', '800m', '1000m',
+'1500m', 'Mile', '2000m', '3000m', '5000m',
+'10000m', 'Half-marathon', 'Marathon', '50km', '100km'])
+
+user_pace_hour = st.number_input('min',
+    min_value=0, max_value=100, value=0, step=1)
+
+user_pace_min = st.number_input('min',
+    min_value=0, max_value=30, value=5, step=1)
+
+user_pace_sec = st.number_input('sec',
+    min_value=0, max_value=59, value=0, step=1)
+
 user_percentage = st.number_input('Set a speed percentage:',
     min_value=1.0, max_value=110.0, value=68.0, step=0.1)
 
 records['User'] = records['Men'] * user_percentage/100
 
-records = records.melt(id_vars='Distance', var_name='Group', value_name='Speed')
+records = records.melt(id_vars=['Event','Distance'], var_name='Group', value_name='Speed')
 
-records['Pace'] = records['Speed'].apply(kmh_to_pace)
+records['Pace'] = records['Speed'].apply(kmh_to_pace, decimals=False)
+
+####################################################
 
 a = alt.Chart(records).mark_circle().encode(
     x=alt.Y('Distance', scale=alt.Scale(type="log")),
--- helpers.py
+++ helpers.py
@@ -1,19 +1,19 @@
 def kmh_to_seckm(x):
      return round(3600/x)
 
-def seckm_to_string(x, decimals):
+def seckm_to_minkm(x, decimals):
     min = f'{int(x//60)}'
     sec = x%60
 
-    if decimals:
-        sec_align = f'{int(sec)}'.zfill(2) + '.' + f'{round(10*(sec-int(sec)))}'
+    if decimals>0:
+        sec_align = f'{int(sec)}'.zfill(2) + '.' + f'{round(10**decimals*(sec-int(sec)))}'
     else:
         sec_align = f'{round(sec)}'.zfill(2)
     return f'{min}:{sec_align}'
 
-def kmh_to_pace(x):
+def kmh_to_pace(x, decimals):
     x = kmh_to_seckm(x)
-    x = seckm_to_string(x)
+    x = seckm_to_minkm(x, decimals)
     return x
 
 class Pace:
@@ -31,8 +31,16 @@ class Pace:
         km = meters/1000
         self.pace = time/km
 
-    def print(self):
-        return seckm_to_string(self.pace, decimals=False)
+    def print(self, decimals=0, unit='minkm'):
+
+        if unit=='minkm':
+            return seckm_to_minkm(self.pace, decimals=decimals)
+        elif unit=='kmh':
+            out = 3600/self.pace
+            if decimals > 0:
+                return round(out, decimals)
+            else:
+                return round(out)
 
 
 
